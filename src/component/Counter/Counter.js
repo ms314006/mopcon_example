b568aa61459668edc30aac085311c7f4f7ee2230
GQSM <ms314006@hotmail.com>
2019-10-21 07:36:32 +0800

    [ADD] 為 Component 的 Element 加上測試特徵 data-testid

--- src/component/Counter/Counter.js
+++ src/component/Counter/Counter.js
@@ -4,14 +4,21 @@ import styles from './index.scss';
 const Counter = () => {
   const [count, setCount] = useState(0);
   return (
-    <div className={styles.counter}>
-      <div className={styles.counterText}>
+    <div
+      className={styles.counter}
+      data-testid="counter"
+    >
+      <div
+        className={styles.counterText}
+        data-testid="counterText"
+      >
         {count}
       </div>
       <button
         type="button"
         className={styles.counterButton}
         onClick={() => { setCount(count + 1); }}
+        data-testid="counterButton"
       >
         點我加 1
       </button>

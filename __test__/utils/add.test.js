0d2aab88e19df52684223b3cdbc04694d5a3ae4e
GQSM <ms314006@hotmail.com>
2019-10-21 07:41:50 +0800

    [ADD] 替 add 的測試案例增加註解

--- __test__/utils/add.test.js
+++ __test__/utils/add.test.js
@@ -5,19 +5,33 @@ import add from '../../src/utils/add';
 // Ｘ add_UseTwoNumbers_RrturnSum
 
 test('add_useParam1and1_return2', () => {
+  // Arrange 安排
   const expected = 2;
+
+  // Act 作為
   const actual = add(1, 1);
+
+  // Assert 斷言
+  // 1. 確認回傳結果
+  // 2. 確認狀態改變
+  // 3. 確認某個函式是否有執行
   expect(actual).toBe(expected);
 });
 
 test('add_useStringType_willThrowError', () => {
+  // Arrange
   const expected = 'The parameter type is not number!';
+  // Act
   const actual = () => { add('1', 1); };
+  // Assert
   expect(actual).toThrowError(expected);
 });
 
 test('add_useOneParame_willThrowError', () => {
+  // Arrange
   const expected = 'The parameter type is not number!';
+  // Act
   const actual = () => { add(1); };
+  // Assert
   expect(actual).toThrowError(expected);
 });

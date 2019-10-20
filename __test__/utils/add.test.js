1f29d68788d4f5f36b475403480ca8d973aa3059
GQSM <ms314006@hotmail.com>
2019-10-21 07:39:05 +0800

    [CHANGE] 修改 add 的錯誤訊息內容，整理測試案例

--- __test__/utils/add.test.js
+++ __test__/utils/add.test.js
@@ -1,5 +1,9 @@
 import add from '../../src/utils/add';
 
+// 對誰做＿做了什麼（怎麼做）＿期望結果
+// Ｏ add_UseParam1and1_Return2
+// Ｘ add_UseTwoNumbers_RrturnSum
+
 test('add_useParam1and1_return2', () => {
   const expected = 2;
   const actual = add(1, 1);
@@ -7,13 +11,13 @@ test('add_useParam1and1_return2', () => {
 });
 
 test('add_useStringType_willThrowError', () => {
-  const expected = 'The parameter is undefined or type is not number!';
+  const expected = 'The parameter type is not number!';
   const actual = () => { add('1', 1); };
   expect(actual).toThrowError(expected);
 });
 
 test('add_useOneParame_willThrowError', () => {
-  const expected = 'The parameter is undefined or type is not number!';
+  const expected = 'The parameter type is not number!';
   const actual = () => { add(1); };
   expect(actual).toThrowError(expected);
 });
--- src/utils/add.js
+++ src/utils/add.js
@@ -1,6 +1,6 @@
 const add = (number1, number2) => {
   if (typeof number1 !== 'number' || typeof number2 !== 'number') {
-    throw new Error('The parameter is undefined or type is not number!');
+    throw new Error('The parameter type is not number!');
   }
   return number1 + number2;
 };
